2e407dcb55373dfee0cf4a06fa05df44bdfb237a
ivmaksimov <100725158+ivmaksimov@users.noreply.github.com>
2022-03-17 19:33:12 +0100

    Add files via upload

--- chalenge/details.php
+++ chalenge/details.php
@@ -23,7 +23,7 @@ mysqli_close($connect);
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <title>details</title>
        <?php require_once './components/boot.php'?>
-       
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
        
        
    </head>
